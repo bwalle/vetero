f6dd2a605a712ca856900213e9aed3196ca888f7
Bernhard Walle <bernhard@bwalle.de>
2012-01-04 17:39:28 +0100

    scripts/pressure.py: Use the formula from the data sheet
    Using this formula the results from the BMP085 are actually correct comparing
    it with the official weather data from Airport Nürnberg. The hardware seems to
    compensate the temperature already...

--- scripts/pressure.py
+++ scripts/pressure.py
@@ -1,46 +1,24 @@
 #!/usr/bin/python3
 
 import sys
-import math
 
 def usage():
-    print('Usage: pressure.py <height [m]> <pressure [hPa]> <temperature [°C]>')
+    print('Usage: pressure.py <height [m]> <pressure [hPa]>')
     sys.exit(1)
 
-def calc_E(temperature):
-    """Temperature must be in °C"""
-    if temperature < 9.1:
-        return 5.6402 * (-0.0916 + math.exp(0.06*temperature))
-    else:
-        return 18.2194 * (1.0463 - math.exp(-0.0666*temperature))
+def calculate_pressure(height, abspressure):
+    """See the data sheet of the BMP085 for the formula"""
 
-def C_to_K(temperature):
-    """Converts Celsius to Kelvin"""
-    return temperature + 273.25
-
-def calculate_pressure(height, abspressure, temperature):
-    """See http://de.wikipedia.org/wiki/Barometrische_H%C3%B6henformel#Reduktion_auf_Meeresh.C3.B6he"""
-
-    # constants
-    g0 = 9.80665
-    R = 287.05
-    a = 0.0065
-    Ch = 0.12
-    E = calc_E(temperature)
-    Th = C_to_K(temperature)
-    x = g0 / (R * (Th + Ch*E + a*height/2.0)) * height
-
-    return abspressure * math.exp(x)
+    return abspressure / ((1 - height/44330.0) ** 5.255)
 
 def main():
-    if len(sys.argv) != 4:
+    if len(sys.argv) != 3:
         usage()
 
     height = float(sys.argv[1])
     abspressure = float(sys.argv[2])
-    temperature = float(sys.argv[3])
 
-    pressure = calculate_pressure(height, abspressure, temperature)
+    pressure = calculate_pressure(height, abspressure)
     print('Relative pressure: %f hPa' % (pressure))
 
 if __name__ == "__main__":
